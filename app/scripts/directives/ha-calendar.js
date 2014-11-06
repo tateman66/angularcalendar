0cbf5e8298f43907512742180ef16a73ba958454
Steven Tate <steven.tate@icicleinnovations.com>
2014-11-05 18:49:35 -0700

    animation

--- app/scripts/directives/ha-calendar.js
+++ app/scripts/directives/ha-calendar.js
@@ -7,7 +7,7 @@
  * # haCalendar
  */
  angular.module('angularcalendarApp')
- .directive('haCalendar', function (dateUtils, $timeout) {
+ .directive('haCalendar', function (dateUtils) {
     return {
         templateUrl: 'views/ha-calendar.html',
         restrict: 'E',
@@ -16,22 +16,67 @@
             scope.weekdays = scope.weekdays || dateUtils.getDaysOfWeek();
             scope.limitIndex = 2;
             scope.limitTrailer = 2;
+            scope.animating = false;
 
-            var monthsToDisplay = 2;
+            var monthsToDisplay = 2,
+                ol = document.getElementById('months'),
+                transitionEnd = transitionEnd();
 
             scope.next = function(){
 
-
                 scope.limitIndex++;
-                scope.limitTrailer = -1*monthsToDisplay;
+                scope.limitTrailer = -2;
+
+                scope.animating = true;
+                ol.style.left = '-52%';
+
+                if (transitionEnd){
+                    ol.addEventListener(transitionEnd, onTransitionEnd);
+                } else {
+                    onTransitionEnd();
+                }
             };
 
             scope.prev = function(){
-
-
                 scope.limitIndex--;
-                scope.limitTrailer = (scope.limitIndex !== monthsToDisplay) ? -1*monthsToDisplay : monthsToDisplay;
+                scope.limitTrailer = scope.limitIndex;
+
+                scope.animating = true;
+                ol.style.left = '0%';
+
+                if (transitionEnd){
+                    ol.addEventListener(transitionEnd, onTransitionEnd);
+                } else {
+                    onTransitionEnd();
+                }
             };
+
+            function onTransitionEnd(){
+                scope.$apply(function(){
+                    scope.animating = false;
+                    scope.limitTrailer = -1*monthsToDisplay;
+                    ol.style.left = '0%';
+                });
+            }
+
+            function transitionEnd() {
+                var el = document.createElement('div');
+
+                var transEndEventNames = {
+                    WebkitTransition : 'webkitTransitionEnd',
+                    MozTransition    : 'transitionend',
+                    OTransition      : 'oTransitionEnd otransitionend',
+                    transition       : 'transitionend'
+                };
+
+                for (var name in transEndEventNames) {
+                    if (el.style[name] !== undefined) {
+                        return transEndEventNames[name];
+                    }
+                }
+
+                return false; // explicit for ie8 (  ._.)
+            }
         }
     };
- });
+});
