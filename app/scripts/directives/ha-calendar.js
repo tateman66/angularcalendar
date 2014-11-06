17d35ec012cdfb6423a65f875d660f631f37e0ad
Steven Tate <steven.tate@icicleinnovations.com>
2014-11-05 17:49:06 -0800

    first and last dates of prior next month removed

--- app/scripts/directives/ha-calendar.js
+++ app/scripts/directives/ha-calendar.js
@@ -6,32 +6,42 @@
  * @description
  * # haCalendar
  */
- angular.module('angularcalendarApp')
- .directive('haCalendar', function (dateUtils, $timeout) {
+angular.module('angularcalendarApp')
+  .directive('haCalendar', function (dateUtils, $timeout) {
     return {
-        templateUrl: 'views/ha-calendar.html',
-        restrict: 'E',
-        link: function(scope){
-            scope.months = dateUtils.getVisibleMonths();
-            scope.weekdays = scope.weekdays || dateUtils.getDaysOfWeek();
-            scope.limitIndex = 2;
-            scope.limitTrailer = 2;
+      templateUrl: 'views/ha-calendar.html',
+      restrict: 'E',
+      link: function(scope){
+        scope.months = dateUtils.getVisibleMonths();
+        scope.weekdays = scope.weekdays || dateUtils.getDaysOfWeek();
+        scope.limitIndex = 2;
+        scope.limitTrailer = 2;
 
-            var monthsToDisplay = 2;
+        var monthsToDisplay = 2;
 
-            scope.next = function(){
-
-
-                scope.limitIndex++;
-                scope.limitTrailer = -1*monthsToDisplay;
-            };
-
-            scope.prev = function(){
+        scope.next = function(){
 
 
-                scope.limitIndex--;
-                scope.limitTrailer = (scope.limitIndex !== monthsToDisplay) ? -1*monthsToDisplay : monthsToDisplay;
-            };
+          scope.limitIndex++;
+          scope.limitTrailer = -1*monthsToDisplay;
+        };
+
+        scope.prev = function(){
+
+
+          scope.limitIndex--;
+          scope.limitTrailer = (scope.limitIndex !== monthsToDisplay) ? -1*monthsToDisplay : monthsToDisplay;
+        };
+
+        scope.displayDay = function(d, f, l) {
+          if ((!(f && d.getDate() > 7)) &&
+          (!(l && d.getDate() < 14))) {
+            return d.getDate()
+          } else {
+            return '_';
+          }
+
         }
+      }
     };
- });
+  });
--- app/scripts/services/dateUtils.js
+++ app/scripts/services/dateUtils.js
@@ -1,88 +1,88 @@
 'use strict';
 
 angular.module('angularcalendarApp').factory('dateUtils', function(){
-	return {
-		getVisibleMonths : function(){
-			var today = new Date(),
-    			currentMonth = today.getMonth(),
-    			currentYear = today.getFullYear(),
-    			months = [],
-                monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  return {
+    getVisibleMonths : function(){
+      var today = new Date(),
+        currentMonth = today.getMonth(),
+        currentYear = today.getFullYear(),
+        months = [],
+        monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-			for (var i=0; i<12; i++){
+      for (var i=0; i<12; i++){
 
-                if (currentMonth === 12){
-                    currentMonth = 0;
-                    currentYear++;
-                }
+        if (currentMonth === 12){
+          currentMonth = 0;
+          currentYear++;
+        }
 
-                months.push({
-                    weeks: this.getVisibleWeeks(new Date(currentYear, currentMonth, 1)),
-                    name: monthNames[currentMonth] + ' ' + currentYear
-                });
+        months.push({
+          weeks: this.getVisibleWeeks(new Date(currentYear, currentMonth, 1)),
+          name: monthNames[currentMonth] + ' ' + currentYear
+        });
 
-				currentYear = (currentMonth > 11) ? currentYear + 1 : currentYear;
-                currentMonth = (currentMonth > 11) ? currentMonth-12 : ++currentMonth;
-			}
+        currentYear = (currentMonth > 11) ? currentYear + 1 : currentYear;
+        currentMonth = (currentMonth > 11) ? currentMonth-12 : ++currentMonth;
+      }
 
-			return months;
-		},
-		getVisibleWeeks : function(date) {
-			date = new Date(date || new Date());
-			var startMonth = date.getMonth(), startYear = date.getYear();
-			date.setDate(1);
-			date.setHours(0);
-			date.setMinutes(0);
-			date.setSeconds(0);
-			date.setMilliseconds(0);
+      return months;
+    },
+    getVisibleWeeks : function(date) {
+      date = new Date(date || new Date());
+      var startMonth = date.getMonth(), startYear = date.getYear();
+      date.setDate(1);
+      date.setHours(0);
+      date.setMinutes(0);
+      date.setSeconds(0);
+      date.setMilliseconds(0);
 
-			if (date.getDay() === 0) {
-				date.setDate(-5);
-			} else {
-				date.setDate(date.getDate() - (date.getDay()));
-			}
-			if (date.getDate() === 1) {
-				date.setDate(-6);
-			}
+      if (date.getDay() === 0) {
+        date.setDate(-5);
+      } else {
+        date.setDate(date.getDate() - (date.getDay()));
+      }
+      if (date.getDate() === 1) {
+        date.setDate(-6);
+      }
 
-			var weeks = [];
-			while (weeks.length < 6) {
-				/*jshint -W116 */
-				if(date.getYear()=== startYear && date.getMonth() > startMonth) break;
-				var week = [];
-				for (var i = 0; i < 7; i++) {
-					week.push(new Date(date));
-					date.setDate(date.getDate() + 1);
-				}
-				weeks.push(week);
-			}
-			return weeks;
-		},
-		getDaysOfWeek : function(date) {
-			date = new Date(date || new Date());
-			date = new Date(date.getFullYear(), date.getMonth(), date.getDate());
-			date.setDate(date.getDate() - (date.getDay()));
-			var days = [];
-			for (var i = 0; i < 7; i++) {
-				days.push(new Date(date));
-				date.setDate(date.getDate() + 1);
-			}
-			return days;
-		},
-		isAfter : function(model, date) {
-			return model && model.getTime() <= date.getTime();
-		},
-		isBefore : function(model, date) {
-			return model.getTime() >= date.getTime();
-		},
-		isSameYear :   function(model, date) {
-			return model && model.getFullYear() === date.getFullYear();
-		},
-		isSameMonth : function(model, date) {
-			return this.isSameYear(model, date) && model.getMonth() === date.getMonth();
-		},
-		isSameDay : function(model, date) {
-			return this.isSameMonth(model, date) && model.getDate() === date.getDate();
-		}
-	};
+      var weeks = [];
+      while (weeks.length < 6) {
+        /*jshint -W116 */
+        //if(date.getYear()=== startYear && date.getMonth() > startMonth) break;
+        var week = [];
+        for (var i = 0; i < 7; i++) {
+          week.push(new Date(date));
+          date.setDate(date.getDate() + 1);
+        }
+        weeks.push(week);
+      }
+      return weeks;
+    },
+    getDaysOfWeek : function(date) {
+      date = new Date(date || new Date());
+      date = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+      date.setDate(date.getDate() - (date.getDay()));
+      var days = [];
+      for (var i = 0; i < 7; i++) {
+        days.push(new Date(date));
+        date.setDate(date.getDate() + 1);
+      }
+      return days;
+    },
+    isAfter : function(model, date) {
+      return model && model.getTime() <= date.getTime();
+    },
+    isBefore : function(model, date) {
+      return model.getTime() >= date.getTime();
+    },
+    isSameYear :   function(model, date) {
+      return model && model.getFullYear() === date.getFullYear();
+    },
+    isSameMonth : function(model, date) {
+      return this.isSameYear(model, date) && model.getMonth() === date.getMonth();
+    },
+    isSameDay : function(model, date) {
+      return this.isSameMonth(model, date) && model.getDate() === date.getDate();
+    }
+  };
 });
