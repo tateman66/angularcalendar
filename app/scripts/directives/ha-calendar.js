004e2d116173b1e8139f5c10d4623595a3455633
Steven Tate <steven.tate@icicleinnovations.com>
2014-11-05 17:15:27 -0700

    prev/next

--- app/scripts/directives/ha-calendar.js
+++ app/scripts/directives/ha-calendar.js
@@ -7,7 +7,7 @@
  * # haCalendar
  */
  angular.module('angularcalendarApp')
- .directive('haCalendar', function (dateUtils) {
+ .directive('haCalendar', function (dateUtils, $timeout) {
     return {
         templateUrl: 'views/ha-calendar.html',
         restrict: 'E',
@@ -20,11 +20,15 @@
             var monthsToDisplay = 2;
 
             scope.next = function(){
+
+
                 scope.limitIndex++;
                 scope.limitTrailer = -1*monthsToDisplay;
             };
 
             scope.prev = function(){
+
+
                 scope.limitIndex--;
                 scope.limitTrailer = (scope.limitIndex !== monthsToDisplay) ? -1*monthsToDisplay : monthsToDisplay;
             };
